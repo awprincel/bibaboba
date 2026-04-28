61de6064cc7f463d135ae32e2413a6479bb0387d
Nikita Mapin <n1ky0802@mail.ru>
2026-04-28 11:21:03 +0300

    WS-1

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './app/router'
+import { Provider } from 'react-redux'
+import { store } from './app/provider/store'
 
 createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
---
+++ src/shared/config/enums.ts
@@ -0,0 +1,16 @@
+export enum ELSNames {
+    ACCESS_TOKEN = "accessToken",
+    REFRESH_TOKEN = "refreshToken",
+    USER = "user"
+}
+
+export enum ERoutePaths {
+    HOME = "home",
+    REGISTER = "register",
+    LOGIN = "login",
+    SPACES = "spaces",
+    SPACE = "space",
+    BOOKINGS = "bookings",
+    BOOKING = "booking",
+    NOT_FOUND = "notFound",
+}
