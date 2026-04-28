f7aa474fe94b710d2495fce77446995d3de704e6
Nikita Mapin <n1ky0802@mail.ru>
2026-04-28 10:06:08 +0300

    added delete btn

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 const button = document.getElementById('cola-button')
+const closeButton = document.getElementById('cola-button-close')
 const container = document.getElementById('container')
 const text = document.getElementById('text')
 
 button.addEventListener('click', () => {
     text.textContent = "Вы попили колы"
     container.appendChild(text)
+})
+
+closeButton.addEventListener('click', () => {
+    text.textContent = ""
 })
